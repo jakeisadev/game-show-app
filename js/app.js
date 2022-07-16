8de6eb63939050822bbedfe028c173dd8561e130
jakeisadev <starowicz@protonmail.com>
2022-07-16 18:45:57 -0400

    created working  randomPhraseArray

--- js/app.js
+++ js/app.js
@@ -2,16 +2,27 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const overlay = document.getElementById('overlay');
 const btn__reset = document.getElementById('start');
+const randomNumber = () => Math.floor(Math.random() * 5);
+const phrasesList = [
+    'it do be like that sometimes',
+    'only on wednesdays',
+    'peaky blinders',
+    'code geass',
+    'hunter x hunter'
+];
 let missed = 0;
 
 //return a random phrase from an array
 const getRandomPhraseArray = arr => {
-
+    let randomArray = arr[randomNumber()];
+    return splitArray = randomArray.split([,]);
 }
+
+const splitRandomPhrase = getRandomPhraseArray(phrasesList); //array of letters gets captured by a variable
 
 //adds the letters of a string to the display
 const addPhraseToDisplay = arr => {
-    
+
 }
 
 //check if a letter is in the phrase
