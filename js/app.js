eb5afe54fc687822ad97a084dc983e2fbd7e8a16
jakeisadev <starowicz@protonmail.com>
2022-07-16 16:26:05 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const overlay = document.getElementById('overlay');
+const btn__reset = document.getElementById('start');
+let missed = 0;
+
+//return a random phrase from an array
+const getRandomPhraseArray = arr => {
+
+}
+
+//adds the letters of a string to the display
+const addPhraseToDisplay = arr => {
+    
+}
+
+//check if a letter is in the phrase
+const checkLetter = () => {
+
+}
+
+//check if the game has been won or lost
+const checkWin = () => {
+
+}
+
+//listen for the start game button to be pressed
+btn__reset.addEventListener('click', () => {
+    overlay.style.display = 'none';
+})
+
+//listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', () => {
+
+})
