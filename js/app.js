2ed922928e1b04311ec4ac280ca934775bb58c15
jakeisadev <starowicz@protonmail.com>
2022-07-16 19:23:47 -0400

    added more variables and function to display random phrase on screen

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const overlay = document.getElementById('overlay');
 const btn__reset = document.getElementById('start');
-const randomNumber = () => Math.floor(Math.random() * 5);
 const phrasesList = [
     'it do be like that sometimes',
     'only on wednesdays',
@@ -12,6 +11,17 @@ const phrasesList = [
 ];
 let missed = 0;
 
+const win = document.createElement('p');
+win.classList.add('message');
+win.textContent = 'Congratulations!';
+
+const lose = document.createElement('p');
+lose.classList.add('message');
+lose.textContent = 'You Lost. Play again?';
+
+let guessesMissed, letterFound;
+
+const randomNumber = () => Math.floor(Math.random() * 5);
 //return a random phrase from an array
 const getRandomPhraseArray = arr => {
     let randomArray = arr[randomNumber()];
@@ -21,14 +31,46 @@ const getRandomPhraseArray = arr => {
 const splitRandomPhrase = getRandomPhraseArray(phrasesList); //array of letters gets captured by a variable
 
 //adds the letters of a string to the display
-const addPhraseToDisplay = arr => {
+const addPhraseToDisplay = (arr) => {
+     // loop through array of characters
+  for (let i = 0; i < arr.length; i++) {
+    const letter = arr[i];
+    // For each character, create a list item
+    const item = document.createElement('li');
+    // Put each character inside the list item
+    item.textContent = letter;
 
+    // Add the appropriate class to the list items
+    if (letter !== " ") {
+      item.className = 'letter';
+    } else {
+      item.className = 'space';
+    }
+
+    // Append the list item to the DOM (specifically to the #phrase ul )
+    ul.appendChild(item);
+  }
 }
 
 //check if a letter is in the phrase
-const checkLetter = () => {
+const checkLetter = (buttonClicked) => { //Removes
+      // Loop through the characters in the phrase
+  for (let i = 0; i < li.length; i++) {
+    // Make sure a letter is chosen
+    if ( li[i].classList.contains('letter') ) {
 
+        // Check the textContent of the button to see if there's a match
+        if (li[i].textContent === guessBtn) {
+          // Add the 'show' class
+          li[i].classList.add('show');
+          // Save the correct guess
+          letterFound = guessBtn;
+        }
+    }
+  }
 }
+
+//targets button clicked then changs it to different color
 
 //check if the game has been won or lost
 const checkWin = () => {
@@ -44,3 +86,7 @@ btn__reset.addEventListener('click', () => {
 qwerty.addEventListener('click', () => {
 
 })
+
+const reload = () => {
+    window.location.reload();
+}
